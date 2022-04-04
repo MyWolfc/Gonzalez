7fddde6ea4d56687c7547f4ae7dcb0757d3f5575
MyWolfc <juanitoamerica1@hotmail.com>
2022-04-04 14:43:47 -0500

    ACTUALIZACION EPICA

---
+++ parcial 2/barralateralJQ/Baralatera.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    //document.getElementById("btn").addEventListener("click",Alternar);
+    $("#btn").Alternar;
+    function Alternar(){
+
+        //document.getElementById("SIDE").classList.toggle("Cerrar");
+        $("SIDE").classlist.toglle("Cerrar");
+        //document.getElementById("CONT").classList.toggle("contenidos");
+        $("CONT").classlist.toglle("Contenidos");
+    }
+
+
+}
