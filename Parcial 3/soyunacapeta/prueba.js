9b7ae832818ba9a407eb1827ea024a915d22bbaf
MyWolfc <juanitoamerica1@hotmail.com>
2022-05-02 18:52:18 -0500

    actualizacion de tarea

---
+++ Parcial 3/soyunacapeta/prueba.js
@@ -0,0 +1,7 @@
+import swal from 'sweetalert';
+
+$(document).ready(function(){
+    $('#Botoncito').click(function(event){
+        swal("Here's the title!", "...and here's the text!");
+    });
+});
